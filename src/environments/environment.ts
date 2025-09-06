be46716a3a88a991652f08c9f43c13976980aaa1
Colin Michaels <colin@colinmichaels.com>
2025-09-06 15:54:37 -0400

    ### Add automated Firebase deployment workflow and environment variable handling
    - Created a GitHub Actions workflow (`firebase_deployment_workflow.yml`) to automatically deploy the application to Firebase Hosting on pushes to the `main` branch.
      - Includes steps for Node.js setup, dependency installation, build process, and deployment.
      - Added secure integration via GitHub Secrets for sensitive environment variables.
    - Updated `environment.ts` to use environment variables for Firebase and third-party API configurations, removing hard-coded sensitive data.
    - Refined `.gitignore` to exclude `.env` files and environment-specific configuration files for better security and maintainability.
    
    **Signed-off-by:** Colin Michaels <colin@colinmichaels.com>
    
    Signed-off-by: Colin Michaels <colin@colinmichaels.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
-  title: 'Colin Michaels - LOCAL',
-  apiUrl: 'http://localhost:3000', // your NestJS backend
-  openAiApiKey: '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',
-  openWeatherMapApiKey: '94280521bd9339761dcf9ad73816cb64',
+  title: process.env['APP_TITLE'] || 'Colin Michaels',
+  apiUrl: process.env['API_URL'] || 'http://localhost:3000',
+  openAiApiKey: process.env['OPENAI_API_KEY'] || '',
+  openWeatherMapApiKey: process.env['OPEN_WEATHER_MAP_API_KEY'] || '',
   firebaseConfig: {
-    apiKey: "AIzaSyDxAfhvfwY2g3jLHHRPewj8NMgj9P0PP_Y",
-    authDomain: "colinmichaels.firebaseapp.com",
-    databaseURL: "https://colinmichaels-default-rtdb.firebaseio.com/",
-    projectId: "colinmichaels",
-    storageBucket: "colinmichaels.firebasestorage.app",
-    messagingSenderId: "695739708994",
-    appId: "1:695739708994:web:0a44e9b2d8d614b617dfdb",
-    measurementId: "G-MWYBVKZ7KE"
+    apiKey: process.env['FIREBASE_API_KEY'] || '',
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
+    databaseURL: process.env['FIREBASE_DATABASE_URL'] || '',
+    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['FIREBASE_APP_ID'] || '',
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || ''
   }
 };
